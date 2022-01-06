fc0af27f0324c8c96c1d74cce5cdb7ce8c422285
laurence <laurence@tasker.ph>
2022-01-07 07:17:53 +0800

    added new functions for first letter, cleaned up some code

--- lesson_2/rock_paper_scissors_lizard_spock.js
+++ lesson_2/rock_paper_scissors_lizard_spock.js
@@ -2,6 +2,8 @@ const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 let computerChoice;
 let choice;
+let playAgainChoice = true;
+
 
 function displayWinner(choice, computerChoice) {
   if (userWins(choice, computerChoice)) return 'You win!';
@@ -40,29 +42,57 @@ function computerChoose() {
 }
 
 function userChoose() {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  choice = readline.question();
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    choice = readline.question();
-  }
+  do {
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')} (you can also enter the first letter)`);
+    choice = readline.question().toLowerCase();
+    choice = expandUserChoice(choice);
+    if (!VALID_CHOICES.includes(choice)) prompt("That's not a valid choice. Please try again.");
+  } while (!VALID_CHOICES.includes(choice));
   return choice;
 }
 
-let answer = 'yes';
-while (answer[0] !== 'n') {
+function expandUserChoice(input) {
+  switch (input) {
+    case 'r':
+      input = 'rock';
+      break;
+    case 'p':
+      input = 'paper';
+      break;
+    case 'l':
+      input = 'lizard';
+      break;
+    case 's':
+      prompt("Did you mean SCissors (sc) or SPock (sp)?");
+      break;
+    case "sc":
+      input = 'scissors';
+      break;
+    case "sp":
+      input = 'spock';
+      break;
+  }
+  return input;
+}
+
+function playAgain() {
+  prompt("Do you want to play again (y/n)");
+  let answer = readline.question().toLowerCase();
+  while (answer !== 'n' && answer !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+  if (answer === 'y') playAgainChoice = true;
+  else if (answer === 'n') playAgainChoice = false;
+}
+
+// main code starts here
+
+while (playAgainChoice) {
   console.clear();
   userChoose();
   computerChoose();
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
   prompt(displayWinner(choice, computerChoice));
-
-  prompt("Do you want to play again (y/n)");
-  answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
-  }
-
-  if (answer[0] !== 'y') break;
+  playAgain();
 }
